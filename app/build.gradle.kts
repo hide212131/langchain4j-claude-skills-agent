02698fbc4e50a73fad2570311b57e5f7f1b0b79d
Hideyuki Kagami <hide212131@gmail.com>
2025-10-24 02:18:53 +0900

    feat: Add additional test runtime dependencies for JUnit 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,8 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.assertj:assertj-core:3.25.3")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
